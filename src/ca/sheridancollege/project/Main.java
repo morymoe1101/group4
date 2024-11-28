2cc4852304d46848bf2e804ba989d825157c7e18
Anmol Pal Singh <sin15077@sheridancollege.ca>
2024-11-28 11:27:30 -0500

    Final Commit for the deliverable 3 for Main class

--- src/ca/sheridancollege/project/Main.java
+++ src/ca/sheridancollege/project/Main.java
@@ -1,20 +1,21 @@
 /**
- * The entry point for the Blackjack game.
- * Initializes players and starts the game.
- * Author: [Your Name]
- * Date: [Today's Date]
+ * The entry point for the Blackjack game. Initializes players and starts the game.
+ *Author: Group3
+ 
  */
 package ca.sheridancollege.project;
 
 public class Main {
 
     public static void main(String[] args) {
-        Game game = new Game("Blackjack");
+        // Get the instance of the game using Singleton
+        Game game = Game.getInstance("Blackjack");
+        // Add players to the game
         game.addPlayer(new Player("Player 1"));
         game.addPlayer(new Player("Player 2"));
 
+        // Start the game and declare the winner
         game.play();
         game.declareWinner();
     }
 }
-
